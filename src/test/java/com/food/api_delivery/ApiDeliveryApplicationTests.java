f8f8189ba2953e0e8966cce82c7e67532cb4213e
De Sousa Paiva <f.de.sousa.paiva@accenture.com>
2026-07-30 23:22:14 -0300

    Initial commit

---
+++ src/test/java/com/food/api_delivery/ApiDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.api_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
